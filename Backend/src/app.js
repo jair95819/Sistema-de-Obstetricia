484b7d59e30df57ed7e030e50d0899e907bb3062
jair <jairestebanca@gmail.com>
2025-12-02 23:48:55 -0500

    ----

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,7 +3,6 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
-import taskRoutes from "./routes/tasks.routes.js";
 
 const app = express();
 
@@ -17,5 +16,4 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", authRoutes);
-app.use("/api", taskRoutes);
 export default app;
